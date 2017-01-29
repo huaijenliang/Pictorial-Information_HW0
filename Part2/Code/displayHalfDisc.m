29cc2dcb4df07956bfbfcf9d18cebf087550cbcf
huaijenliang <r81810g@gmail.com>
2017-01-29 15:29:13 -0500

    modified display

--- Part2/Code/displayHalfDisc.m
+++ Part2/Code/displayHalfDisc.m
@@ -5,14 +5,14 @@ function [ ff ] = displayHalfDisc( h )
 scale = size(h, 1);
 orient = size(h{1, 1}, 3);
 
-ff = tight_subplot(scale, orient * 2, [.01 .03], [.1 .01], [.01 .01]);
+ff = tight_subplot(scale, orient * 2, [.0001 .003], [.00001 .000001], [.001 .001]);
 
 for s = 1:scale
     for o = 1:orient
         h1 = h{s, 1}(:, :, o);
         h2 = h{s, 2}(:, :, o);
-        axes(ff((s - 1) * orient * 2 + o * 2 - 1)); imshow(h1);
-        axes(ff((s - 1) * orient * 2 + o * 2 )); imshow(h2);
+        axes(ff((s - 1) * orient * 2 + o * 2 - 1)), imshow(h1);
+        axes(ff((s - 1) * orient * 2 + o * 2 )), imshow(h2);
 %         subplot(scale, orient * 2, (s - 1) * orient * 2 + o * 2 - 1), imshow(h1);
 %         subplot(scale, orient * 2, (s - 1) * orient * 2 + o * 2), imshow(h2);
     end
